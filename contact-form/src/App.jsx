13d5adc06b1184fa1fdb59e48e1dd86a689aae1c
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-08-10 19:06:17 +0330

    ending css & add btn

--- contact-form/src/App.jsx
+++ contact-form/src/App.jsx
@@ -7,19 +7,22 @@ function App() {
 
   return (
     <div className="container py-5">
-      <div className="row">
-        <div className="col">
-      <h2>ارسال به مدیریت</h2>
+      <div className="row py-5 justify-content-center">
+        <div className="col-md-4 bg-white p-5 mt-5 ">
+      <h2 className='pb-5 text-center' >ارسال به مدیریت</h2>
       <form >
       <div className="form-group">
-        <input name='name' type="text" />
+        <input name='name' className='form-control p-3' type="text" placeholder='نام'/>
         </div>
         <div className="form-group">
         
-        <input name='email' type="email" />
+        <input input name='email' className='form-control p-3' type="email" placeholder=' ایمیل ' />
         </div>
         <div className="form-group">
-        <textarea name="textarea" id="" cols="30" rows="10"></textarea>
+        <textarea className='form-control p-3' name="message" id="" cols="30" rows="10" placeholder='پیام شما'></textarea>
+      </div>
+      <div className="form-group">
+        <input type='submit' className='btn btn-success w-100 p-2 mt-5'  />
       </div>
       </form>
 
